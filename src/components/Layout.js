4a9ec54780ad2c3d64ab3bae98cfb0a5b9acafab
MARX1108 <boyuan@vt.edu>
2022-04-30 17:17:02 -0400

    frontend setup

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import { Portal, Box, useDisclosure } from "@chakra-ui/react";
 import React, { useState } from "react";
-import Navbar from "./Navbar"
+import Navbar from "./Navbar";
+import Main from "./Main";
 export default function Dashboard(props) {
   return (
     <Box
@@ -10,27 +11,36 @@ export default function Dashboard(props) {
       overflow="auto"
       position="relative"
       maxHeight="100%"
-      w={{ base: "100%", xl: "calc( 100% - 290px )" }}
-      maxWidth={{ base: "100%", xl: "calc( 100% - 290px )" }}
+      w={{ base: "100%", xl: "calc( 100%)" }}
+      maxWidth={{ base: "100%", xl: "calc( 100%)" }}
       transition="all 0.33s cubic-bezier(0.685, 0.0473, 0.346, 1)"
       transitionDuration=".2s, .2s, .35s"
       transitionProperty="top, bottom, width"
       transitionTimingFunction="linear, linear, ease"
+      backgroundColor={"#F5F7FE"}
     >
       <Portal>
         <Box>
           <Navbar
-            // onOpen={onOpen}
-            // logoText={"Horizon UI Dashboard PRO"}
-            // brandText={getActiveRoute(routes)}
-            // secondary={getActiveNavbar(routes)}
-            // message={getActiveNavbarText(routes)}
-            // fixed={fixed}
-            // {...rest}
+          // onOpen={onOpen}
+          // logoText={"Horizon UI Dashboard PRO"}
+          // brandText={getActiveRoute(routes)}
+          // secondary={getActiveNavbar(routes)}
+          // message={getActiveNavbarText(routes)}
+          // fixed={fixed}
+          // {...rest}
           />
         </Box>
       </Portal>
-
+      <Box
+        mx="auto"
+        p={{ base: "20px", md: "30px" }}
+        pe="20px"
+        minH="100vh"
+        pt="50px"
+      >
+        <Main />
+      </Box>
       {/* <Footer /> */}
     </Box>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,54 @@
-import React, { Component } from 'react';
-import { convertBytes } from './helpers';
-import moment from 'moment'
+import {
+  Avatar,
+  Box,
+  Flex,
+  FormLabel,
+  Icon,
+  Select,
+  SimpleGrid,
+  useColorModeValue,
+  Container,
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+} from "@chakra-ui/react";
+import React from "react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
-class Main extends Component {
+export default function Main() {
+  return (
+    <Box
+      mx="100px"
+      p={{ base: "20px", md: "30px" }}
+      minH="100vh"
+      mt="70px"
+      // border={"5px solid"}
+    >
+      <Grid
+        templateRows="repeat(2, 1fr)"
+        templateColumns="repeat(5, 1fr)"
+        gap={4}
+      >
+        <GridItem colSpan={2} bg="white" borderRadius="25px">
+          <Box mx="5" p="10">
+            <Stat>
+              <StatLabel>Balance</StatLabel>
+              <StatNumber>345,670 ETH</StatNumber>
+              <StatHelpText>
+                <StatArrow type="increase" />
+                23.36%
+              </StatHelpText>
+            </Stat>
+          </Box>
+        </GridItem>
 
-  render() {
-    return (
-      <div className="container-fluid mt-5 text-center">
-        <div className="row">
-          <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '1024px' }}>
-            <div className="content">
-              <p>&nbsp;</p>
-              <h1>DStorage starter_code</h1>
-              {/* Creatining uploading card ... */}
-                {/* Uploading file... */}
-              <p>&nbsp;</p>
-              {/* Create Table*/}
-              <table className="table-sm table-bordered text-monospace" style={{ width: '1000px', maxHeight: '450px'}}>
-              {/* Set table columns */}
-                {/* Mapping rows... */}
-              </table>
-            </div>
-          </main>
-        </div>
-      </div>
-    );
-  }
+        <GridItem colSpan={3} bg="white" borderRadius="25px" />
+        <GridItem colSpan={2} bg="white" borderRadius="25px" />
+        <GridItem colSpan={3} bg="white" borderRadius="25px" />
+      </Grid>
+    </Box>
+  );
 }
-
-export default Main;
