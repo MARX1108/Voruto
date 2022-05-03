ab409ef606e3e7b434f47b83834fd0552f09c382
MARX1108 <boyuan@vt.edu>
2022-05-03 12:38:14 -0400

    balance card

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import {
-  Text,
+  Flex,
   Box,
   Stat,
   StatLabel,
@@ -17,26 +17,31 @@ export default function Main(props) {
   return (
     <Box mx="100px" p={{ base: "20px", md: "30px" }} minH="100vh" mt="70px">
       <Grid
-        templateRows="repeat(3, 1fr)"
+        templateRows="300px repeat(3, 1fr)"
         templateColumns="repeat(5, 1fr)"
         gap={4}
       >
-        <GridItem colSpan={2} bg="white" borderRadius="25px">
-          <Box mx="5" p="10">
+        <GridItem
+          colSpan={2}
+          bg="white"
+          borderRadius="25px"
+          bgGradient="linear-gradient(115.07deg, #29E9F5 -9.41%, #7A64FF 28.04%, #FF508B 71.85%, #FD6D53 112.49%, #FD6D53 112.49%)"
+        >
+          <Flex p="10" pt="25%" color={"white"}>
             <Stat>
               <StatLabel>Balance</StatLabel>
-              <StatNumber>
+              <StatNumber fontSize={"1.5rem"}>
                 {props.balance
                   ? window.web3.utils.fromWei(props.balance, "ether")
                   : null}
                 ETH
               </StatNumber>
               <StatHelpText>
-                <StatArrow type="increase" />
+                <StatArrow type="increase" color={"white"} />
                 23.36%
               </StatHelpText>
             </Stat>
-          </Box>
+          </Flex>
         </GridItem>
 
         <GridItem colSpan={3} bg="white" borderRadius="25px">
@@ -46,19 +51,17 @@ export default function Main(props) {
             dataContract={props.dataContract}
           />
         </GridItem>
-
-        <GridItem colSpan={2} bg="white" borderRadius="25px">
-          <AccessHistory
-            account={props.account}
-            dataContract={props.dataContract}
-          />
-        </GridItem>
-
         <GridItem colSpan={3} bg="white" borderRadius="25px">
           <TransactionHistory
             account={props.account}
             dataContract={props.dataContract}
             contracts={props.contracts}
+          />
+        </GridItem>
+        <GridItem colSpan={2} bg="white" borderRadius="25px">
+          <AccessHistory
+            account={props.account}
+            dataContract={props.dataContract}
           />
         </GridItem>
 
